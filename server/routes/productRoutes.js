7c3771e8a7c8675c9216a9a7a825e53e50f22693
PallavSarkar2005 <pallav2005sarkar@gmail.com>
2025-12-30 22:31:59 +0530

    Security: Implemented API Rate Limiting to prevent DDOS and spam

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,9 +1,23 @@
-// Routes for particular product or just product.......
+// Routes for particular product or just product and added rate-limiting.......
 
-const express = require('express');
+import express from "express";
+import rateLimit from "express-rate-limit";
+import { protect, admin } from "../middleware/authMiddleware.js";
+import {getProducts,getProductById} from "../controllers/productController.js";
+
 const router = express.Router();
-const { getProducts, getProductById } = require('../controllers/productController');
-router.route('/').get(getProducts);
-router.route('/:id').get(getProductById);
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: {
+    message:
+      "Too many requests from this IP, please try again after 15 minutes",
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+router.route("/").get(apiLimiter, getProducts);
 
-module.exports = router;
+router.route("/:id").get(getProductById);
+
+export default router;
